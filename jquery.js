84353e4e0ab9afd43d476a61b3e45a6f4fe1a485
thienphanexcalibur <31528474+thienphanexcalibur@users.noreply.github.com>
2017-12-13 21:19:36 +0700

    Updated

--- jquery.js
+++ jquery.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
     var pop = setInterval(function () {
         box.css('height', (Math.random()) * y);
         box.css('width', (Math.random()) * x);
-    }, 650);
+    }, 400);
     $('button').click(function () {
         clearInterval(pop);
         $('body').html('<h1 style="color:black;">You won</h1>');
